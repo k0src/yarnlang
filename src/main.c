4b55c4669b563fe8882f4f274615a66ec53a17a1
k0 <k0@k0src.com>
2024-05-09 14:23:40 -0500

    get tokens

--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include "include/lexer.h"
 
 int main(int argc, char* argv[]) 
 {
-    printf("Hello World!");
+    lexer_T* lexer = initLexer(
+    "yarn : \"Lonnie Liston Smith\"|\n"
+    "say<name>|\n" 
+    ); 
+
+    token_T* token = (void*)0;
+
+    while ((token = getNextToken(lexer)) != (void*)0)
+    {
+        printf("TOKEN(%d, %s)\n", token->type, token->value);
+    }
     return 0;
 }
