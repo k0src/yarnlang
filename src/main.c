fe770b5095fb3764fc27dc287734f97d2c69d651
k0 <k0src@null.net>
2024-05-24 20:59:59 -0500

    readding files

--- src/main.c
+++ src/main.c
@@ -2,20 +2,36 @@
 #include "include/lexer.h"
 #include "include/parser.h"
 #include "include/visitor.h"
+#include "include/io.h"
 
-int main(int argc, char* argv[]) 
+void printHelp()
 {
-    lexer_T* lexer = initLexer(
-    "yarn name : \"Lonnie Liston Smith\"|\n"
-    "yarn otherName : \"LTJ Bukem\"|\n"
-    "say<name, otherName>|\n"
-    "cute<>\n"
-    ); 
+    printf("Usage:\nyarn.out <filename>\n");
+    exit(1);
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2) {
+        printHelp();
+        return 1;
+    }
+
+    char* file_contents = getFileContents(argv[1]);
+    if (file_contents == NULL) {
+        printf("Error: Failed to open file '%s'\n", argv[1]);
+        return 1;
+    }
+
+    lexer_T* lexer = initLexer(file_contents);
 
     parser_T* parser = initParser(lexer);
     AST_T* root = parserParse(parser);
+
     visitor_T* visitor = initVisitor();
     visitorVisit(visitor, root);
+
+    free(file_contents); 
 
     return 0;
 }
